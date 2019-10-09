6a8da61ed7a5d72f47f707d44a298319d265cadb
imac1080 <imac1080@gmail.com>
2019-10-09 19:33:20 +0200

    first

---
+++ m6_p5_xml/src/Curs.java
@@ -0,0 +1,185 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Curs implements Serializable {
+	private String tutor;
+	private ArrayList alumnes;
+	private HashMap moduls;
+
+	public Curs() {
+		this.tutor = "";
+		this.alumnes = new ArrayList();
+		this.moduls = new HashMap();
+	}
+
+	public String donaTutor() {
+		return this.tutor;
+	}
+
+	public void canviaTutor(String nou) {
+		this.tutor = nou;
+	}
+
+	public void afegirAlumne(String nou) {
+		this.alumnes.add(nou);
+	}
+
+	public ArrayList donaAlumnes() {
+		return this.alumnes;
+	}
+
+	public void canviaNomAlumne(int index, String nouNom) {
+		this.alumnes.set(index, nouNom);
+	}
+
+	public void treureAlumne(int index) {
+		this.alumnes.remove(index);
+	}
+
+	public void afegirModul(String codi, Modul m) {
+		this.moduls.put(codi, m);
+	}
+
+	public Modul donaModul(String codi) {
+		Modul m = new Modul();
+		m = (Modul) this.moduls.get(codi);
+		return m;
+	}
+
+	public void treureModul(String codi) {
+		this.moduls.remove(codi);
+	}
+
+	public String omplirCurs(Scanner s) {
+		String llista, c, id;
+		llista = c = id = "";
+		System.out.println("Quin sera el nom del curs?");
+		id = s.next();
+		System.out.println("Quin sera el nom del tutor?");
+		this.tutor = s.next();
+		System.out.println("Vols afegir alumnes (s/n)?");
+		llista = s.next();
+		if (llista.equals("s"))
+			this.omplirAlumnes(s);
+		do {
+			System.out.println("Vols afegir un nou modul(s/n)?");
+			llista = s.next();
+			if (llista.equals("s")) {
+				System.out.println("Quina clau te el modul?");
+				c = s.next();
+				Modul m = new Modul();
+				System.out.println("Vols omplir ara les dades del modul (s/n)?");
+				String sn1 = s.next();
+				if (sn1.equals("s")) {
+					m.omplirModul(s);
+				}
+				this.afegirModul(c, m);
+			}
+		} while (llista.equals("s"));
+		return id;
+	}
+
+	public void omplirAlumnes(Scanner s) {
+		String n = "";
+		System.out.println("Escriu el nom de l'alumne (xx per Sortir)");
+		while (!n.equals("xx")) {
+			n = s.next();
+			if (!n.equals("xx"))
+				this.alumnes.add(n);
+		}
+		System.out.println("LLISTA D'ALUMNES TANCADA");
+	}
+
+	public void mostrarDadesCurs() {
+		System.out.println(":: Nom del tutor: " + this.donaTutor());
+		System.out.println(":: Te " + this.alumnes.size() + " alumnes que son:");
+		for (int i = 0; i < this.alumnes.size(); i++)
+			System.out.println(":: " + this.alumnes.get(i));
+		System.out.println(":: Te " + this.moduls.size() + " moduls que son:");
+		Iterator it = this.moduls.entrySet().iterator(); // (1)
+		while (it.hasNext()) {
+			Map.Entry e = (Map.Entry) it.next();
+			Modul m = new Modul();
+			m = (Modul) e.getValue();
+			System.out.println(":: " + e.getKey() + ":" + m.donaNom());
+			m.llistarUfs();
+		}
+	}
+
+	/*
+	 * (1) L'iterador serveix per poder fer fer un recorregut del HashMap moduls. El
+	 * metode entrySet retorna un Set, que es un conjunt d'elements no repetits, que
+	 * treballarem pero amb Map, una interface que olfereix metodes que ens donen
+	 * cadascuna de les parelles d'objectes i no cadascun d'ells de forma
+	 * independent.
+	 */
+	public void modificarDadesCurs(String id) {
+		int k = 0;
+
+		while (k < 7) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Que vols fer amb el curs?");
+			System.out.println(" 1. Canviar de tutor");
+			System.out.println(" 2. Afegir alumnes a " + id);
+			System.out.println(" 3. Treure alumnes de " + id);
+			System.out.println(" 4. Afegir un nou modul a " + id);
+			System.out.println(" 5. Modificar dades d'un modul");
+			System.out.println(" 6. Eliminar un modul a " + id);
+			System.out.println(" 7. TORNAR");
+			k = sc.nextInt();
+			String codi = "";
+			switch (k) {
+			case 1:
+				System.out.println("Quin es el nom del nou tutor?");
+				codi = sc.next();
+				this.canviaTutor(codi);
+				break;
+			case 2:
+				this.omplirAlumnes(sc);
+				break;
+			case 3:
+				System.out.println("Quin numero te l'alumne?");
+				int b = sc.nextInt();
+				String alumne = (String) this.alumnes.get(b - 1);
+				System.out.println("Vols eliminar a " + alumne + " (s/n)?");
+				String sn2 = sc.next();
+				if (sn2.equals("s"))
+					this.treureAlumne(b - 1);
+				break;
+			case 4:
+				System.out.println("Quina clau te el modul?");
+				codi = sc.next();
+				Modul m = new Modul();
+				m.omplirModul(sc);
+				System.out.println("Vols guardar (s/n)?");
+				String sn = sc.next();
+				if (sn.equals("s")) {
+
+					System.out.println("Entro");
+					this.moduls.put(codi, m);
+					System.out.println("EL MODUL " + codi.toUpperCase() + " HA ESTAT INSERIT");
+				}
+				break;
+			case 5:
+				System.out.println("Quina clau te el modul?");
+				codi = sc.next();
+				Modul mm = new Modul();
+				if (this.moduls.containsKey(codi)) {
+
+					mm = (Modul) this.moduls.get(codi);
+					mm.modificarDadesModul(codi);
+				}
+				break;
+			case 6:
+				System.out.println("Quina clau te el modul?");
+				codi = sc.next();
+				this.moduls.remove(codi);
+				System.out.println("EL MODUL " + codi.toUpperCase() + " HA ESTAT ELIMINAT");
+				break;
+			}
+
+		}
+	}
+}
---
+++ m6_p5_xml/src/Modul.java
@@ -0,0 +1,129 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Modul implements Serializable {
+	private String nom;
+	private String profe;
+	private ArrayList ufs;
+
+	public Modul() {
+		this.profe = this.nom = "";
+		this.ufs = new ArrayList();
+	}
+
+	public String donaNom() {
+		return this.nom;
+	}
+
+	public void canviaNom(String nou) {
+		this.nom = nou;
+	}
+
+	public String donaProfe() {
+		return this.profe;
+	}
+
+	public void canviaProfe(String nom) {
+		this.profe = nom;
+	}
+
+	public void afegirUf(String titol) {
+		// ArrayList ordenats=new ArrayList();
+		this.ufs.add(titol);
+	}
+
+	public ArrayList donaUfs() {
+		return this.ufs;
+	}
+
+	public void llistarUfs() {
+		if (!this.ufs.isEmpty()) {
+			for (int i = 0; i < this.ufs.size(); i++)
+				System.out.println(":: " + this.ufs.get(i));
+		} else {
+			System.out.println(":: Aquest modul no te cap UF especificada");
+		}
+
+	}
+
+	public void canviaUf(int numero, String titol) {
+		if (!this.ufs.isEmpty()) {
+			this.ufs.set(numero - 1, titol);
+		} else {
+			System.out.println("Aquest modul no te cap UF especificada");
+		}
+	}
+
+	public void omplirUFs(Scanner s) {
+		System.out.println("Escriu el nom de les UFs (xx per sortir)");
+		String nuf = "";
+		while (!nuf.equals("xx")) {
+			nuf = s.next();
+			if (!nuf.equals("xx"))
+				this.afegirUf(nuf);
+
+		}
+	}
+
+	public void mostrarDadesModul() {
+		System.out.println("Nom modul: " + this.nom);
+		System.out.println("Professor/a: " + this.profe);
+		System.out.println("LLista d'UFs:");
+		this.llistarUfs();
+	}
+
+	public void omplirModul(Scanner s) {
+		System.out.println("Nom del modul:");
+		this.canviaNom(s.next());
+		System.out.println("Nom profesor/a:");
+		this.canviaProfe(s.next());
+		System.out.println("Vols indicar ara les UFs (s/n)?");
+		String sn = s.next();
+		if (sn.equals("s"))
+			this.omplirUFs(s);
+	}
+
+	public void modificarDadesModul(String id) {
+		int k = 0;
+		while (k < 6) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Que vols fer amb el modul?");
+			System.out.println(" 1. Canviar el nom de " + id);
+			System.out.println(" 2. Canviar de professor");
+			System.out.println(" 3. Afegir UFs a " + id);
+			System.out.println(" 4. Canviar una UF de " + id);
+			System.out.println(" 5. Treure una UF");
+			System.out.println(" 6. TORNAR");
+			k = sc.nextInt();
+			String nom = "";
+			switch (k) {
+			case 1:
+				System.out.println("Quin es el nou nom del modul?");
+				nom = sc.next();
+				this.canviaNom(nom);
+				break;
+			case 2:
+				System.out.println("Quin sera el nom del nou professor?");
+				nom = sc.next();
+				this.canviaProfe(nom);
+				break;
+			case 3:
+				this.omplirUFs(sc);
+				break;
+			case 4:
+				System.out.println("Quina es la UF que vols rectificar?");
+				int r = sc.nextInt();
+				System.out.println("Quin es el nou titol de la UF?");
+				nom = sc.next(nom);
+				this.canviaUf(r - 1, nom);
+				break;
+			case 5:
+				System.out.println("Quina es la UF que vols treure?");
+				int e = sc.nextInt();
+				this.ufs.remove(e - 1);
+				break;
+			}
+		}
+	}
+}
