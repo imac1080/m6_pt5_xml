999f0cb4f94586afd7941257a6744239b70e92cc
imac1080 <38972123+imac1080@users.noreply.github.com>
2019-10-15 12:33:36 +0200

    Update Pt2.java

--- m6_p5_xml/src/Pt2.java
+++ m6_p5_xml/src/Pt2.java
@@ -9,7 +9,7 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 public class Pt2 implements Serializable {
-	private HashMap persona;
+	private static HashMap<Integer, Persona> persones;
 
 	/*
 	 * Un HashMap es una col.lecio on cada node es del tipus (Objecte clau, Objecte
@@ -18,7 +18,7 @@ public class Pt2 implements Serializable {
 	 * tipus Curs.
 	 */
 	public Pt2() {
-		this.persona = new HashMap();
+		this.persones = new HashMap<Integer, Persona>();
 	}
 
 	public void carregarDades(File f) {
@@ -34,43 +34,6 @@ public class Pt2 implements Serializable {
 		}
 	}
 
-	public void guardarDades(File f) {
-//		try {
-//			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
-//			oos.writeObject(persones); // ERROR SI S'UTILITZA THIS
-//			System.out.println("==> LES DADES HAN ESTAT GUARDADES AL FITXER\n");
-//			oos.close();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		try {
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.newDocument();
-			// root element
-			Element rootElement = doc.createElement("persones");
-			doc.appendChild(rootElement);
-
-			for (int i = 0; i < persones.size(); i++) {
-				Element departamento = doc.createElement("persona");
-				rootElement.appendChild(departamento);
-				departamento.appendChild(doc.createTextNode(persones);
-				departamento.appendChild(doc.createTextNode("Rafa Aracil"));
-			}
-
-			// write the content into xml file
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(new File("persones.xml"));
-			transformer.transform(source, result);
-			// Output to console for testing
-			StreamResult consoleResult = new StreamResult(System.out);
-			transformer.transform(source, consoleResult);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 	public int menuPrincipal() {
 		Scanner teclat = new Scanner(System.in);
@@ -104,21 +67,58 @@ public class Pt2 implements Serializable {
 				System.out.println("digues el cognom de la persona");
 				String cognom = dades.nextLine();
 				System.out.println("digues la edat de la persona");
+				int identificador = persones.size();
+				identificador++;
+				int  edat;
 				if (dades.hasNextInt()) {
-					o = dades.nextInt();
-					x.persones.put(id + " " + cognom, new Persona(id, cognom, o));
+					edat = dades.nextInt();
+					x.persones.put(identificador, new Persona(id, cognom, edat));
 					System.out.println("La persona" + id + " " + cognom + " ha estat creada");
 					System.out.println("HIHAN " + x.persones.size() + " PERSONES");
 				} else
 					System.out.println("Error no s'ha creat, error de edat");
 				break;
 			case 2:
+				try {
+					DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+					DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+					Document doc = dBuilder.newDocument();
+					// root element
+					Element rootElement = doc.createElement("persones");
+					doc.appendChild(rootElement);
+					for (int i = 1; i <= persones.size(); i++) {
+						Element departamento = doc.createElement("persona");
+						rootElement.appendChild(departamento);
+						//nombre
+						Element nombre = doc.createElement("nombre");
+						departamento.appendChild(nombre);
+						nombre.appendChild(doc.createTextNode(persones.get(i).getNombre()));
+						//apellido
+						nombre = doc.createElement("apellido");
+						departamento.appendChild(nombre);
+						nombre.appendChild(doc.createTextNode(persones.get(i).getApellido()));
+						//edad
+						nombre = doc.createElement("edad");
+						departamento.appendChild(nombre);
+						nombre.appendChild(doc.createTextNode(String.valueOf(persones.get(i).getEdad())));
+					}
 
+					// write the content into xml file
+					TransformerFactory transformerFactory = TransformerFactory.newInstance();
+					Transformer transformer = transformerFactory.newTransformer();
+					DOMSource source = new DOMSource(doc);
+					StreamResult result = new StreamResult(new File("persones.xml"));
+					transformer.transform(source, result);
+					// Output to console for testing
+					StreamResult consoleResult = new StreamResult(System.out);
+					transformer.transform(source, consoleResult);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				System.out.println("guardat a persones.xml");
 				break;
 			}
 		} while (o < 5);
-		File gd = new File(ruta);
-		x.guardarDades(gd); // Tornem a guardar el HashMap de cursos al fitxer
 		System.out.println("S'HA ACABAT");
 	}
 }
