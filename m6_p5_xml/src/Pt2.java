c1f8dc1a5fe2bd5c367c0a825ca5b4bd59f5adf5
imac1080 <imac1080@gmail.com>
2019-10-21 19:39:41 +0200

    print xml (3)

--- m6_p5_xml/src/Pt2.java
+++ m6_p5_xml/src/Pt2.java
@@ -24,25 +24,26 @@ public class Pt2 implements Serializable {
 	public void carregarDades(File f) {
 		persones.clear();
 		try {
-			FileInputStream fis = new FileInputStream(f);
-			ObjectInputStream ois = new ObjectInputStream(fis);
-			persones = (HashMap) ois.readObject(); // ERROR SI S'UTILITZA THIS
-			System.out.println("==> LES DADES DEL FITXER HA ESTAT CARREGADES\n");
-			ois.close();
+			if (f.exists()) {
+				FileInputStream fis = new FileInputStream(f);
+				ObjectInputStream ois = new ObjectInputStream(fis);
+				persones = (HashMap<Integer, Persona>) ois.readObject(); // ERROR SI S'UTILITZA THIS
+				System.out.println("==> LES DADES DEL FITXER HA ESTAT CARREGADES\n");
+				ois.close();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-
 	public int menuPrincipal() {
 		Scanner teclat = new Scanner(System.in);
 		int opcio = 0;
-		for (int i = 0; i < 50; i++)
-			System.out.println("");
+		System.out.println("");
 		System.out.println("==============================================");
 		System.out.println("1. AFEGIR PERSONA");
 		System.out.println("2. GENERAR XML");
+		System.out.println("3. LLEGIR XML");
 		System.out.println("4. SORTIR");
 		System.out.print("Escull una opcio:");
 		opcio = teclat.nextInt();
@@ -52,11 +53,11 @@ public class Pt2 implements Serializable {
 
 	public static void main(String[] args) throws IOException {
 		Pt2 x = new Pt2();
-		String ruta = "myPeople.dat";
+		String ruta = "persones.xml";
 		String id = "";
 		int o = 0;
 		File cd = new File(ruta);
-		x.carregarDades(cd); // Carreguem les dades que han estat guardades anteriorment
+//		x.carregarDades(cd); // Carreguem les dades que han estat guardades anteriorment
 		do {
 			o = x.menuPrincipal();
 			Scanner dades = new Scanner(System.in);
@@ -69,7 +70,7 @@ public class Pt2 implements Serializable {
 				System.out.println("digues la edat de la persona");
 				int identificador = persones.size();
 				identificador++;
-				int  edat;
+				int edat;
 				if (dades.hasNextInt()) {
 					edat = dades.nextInt();
 					x.persones.put(identificador, new Persona(id, cognom, edat));
@@ -89,15 +90,15 @@ public class Pt2 implements Serializable {
 					for (int i = 1; i <= persones.size(); i++) {
 						Element departamento = doc.createElement("persona");
 						rootElement.appendChild(departamento);
-						//nombre
+						// nombre
 						Element nombre = doc.createElement("nombre");
 						departamento.appendChild(nombre);
 						nombre.appendChild(doc.createTextNode(persones.get(i).getNombre()));
-						//apellido
+						// apellido
 						nombre = doc.createElement("apellido");
 						departamento.appendChild(nombre);
 						nombre.appendChild(doc.createTextNode(persones.get(i).getApellido()));
-						//edad
+						// edad
 						nombre = doc.createElement("edad");
 						departamento.appendChild(nombre);
 						nombre.appendChild(doc.createTextNode(String.valueOf(persones.get(i).getEdad())));
@@ -116,6 +117,34 @@ public class Pt2 implements Serializable {
 					e.printStackTrace();
 				}
 				System.out.println("guardat a persones.xml");
+				break;
+			case 3:
+				try {
+					DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+					DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+					Document doc = dBuilder.parse(new File("persones.xml"));
+					// root element
+					System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+					NodeList nList = doc.getElementsByTagName("persona");
+					System.out.println("----------------------------");
+					for (int temp = 0; temp < nList.getLength(); temp++) {
+						Node nNode = nList.item(temp);
+						if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+							Element eElement = (Element) nNode;
+							System.out.println("\nCurrent Element :" + nNode.getNodeName());
+							System.out.println("Nom de la persona : "
+									+ eElement.getElementsByTagName("nombre").item(0).getTextContent());
+							System.out.println("Cognom de la persona : "
+									+ eElement.getElementsByTagName("apellido").item(0).getTextContent());
+							System.out.println("Edat de la persona : "
+									+ eElement.getElementsByTagName("edad").item(0).getTextContent());
+							System.out.println("----------------------");
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				System.out.println("Llegit a persones.xml");
 				break;
 			}
 		} while (o < 5);
